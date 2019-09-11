e7c3a42eb34a4b2229d14dbbccdba7399c05b3a8
williamzhang88 <49259292+williamzhang88@users.noreply.github.com>
2019-09-10 23:07:40 -0400

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,25 @@
+    public List<String> fizzBuzz(int n) 
+    {
+        List<String> b = new ArrayList<String>();
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 ==0)
+            {
+                b.add("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                b.add("Fizz");
+            }
+            else if ( i % 5 ==0)
+            {
+                b.add("Buzz");
+            }
+            else
+            {
+                String s = Integer.toString(i);
+                b.add(s);
+            }
+        }
+        return b;
+    }
